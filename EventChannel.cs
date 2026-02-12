644d3b5e295e808ab3db463d78aef46add3f50f7
mkislenko <mkislenko@gmail.com>
2026-02-12 19:25:49 +0200

    init

---
+++ EventChannel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace RadioDecadance.EventBus
+{
+    [CreateAssetMenu(fileName = "EventChannel", menuName = "Events/EventChannel")]
+    public class EventChannel : ScriptableObject
+    {
+        public event UnityAction OnEventRaised;
+
+        public void RaiseEvent()
+        {
+            OnEventRaised?.Invoke();
+        }
+    }
+
+    public class EventChannel<T> : ScriptableObject
+    {
+        public event UnityAction<T> OnEventRaised;
+        
+        public void RaiseEvent(T data)
+        {
+            OnEventRaised?.Invoke(data);
+        }
+    }
+}
---
+++ EventChannelListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace RadioDecadance.EventBus
+{
+    public class EventChannelListener : MonoBehaviour
+    {
+        [SerializeField] private EventChannel eventChannel;
+        [SerializeField] private UnityEvent response;
+
+        private void OnEnable()
+        {
+            if (eventChannel != null)
+            {
+                eventChannel.OnEventRaised += HandleEventRaised;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (eventChannel != null)
+            {
+                eventChannel.OnEventRaised -= HandleEventRaised;
+            }
+        }
+
+        private void HandleEventRaised()
+        {
+            response?.Invoke();
+        }
+    }
+}
